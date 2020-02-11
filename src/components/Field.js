68c3157ce92c75b82f4f082315c520414600e5b6
realname <realemailaddress@abc.com>
2020-02-11 18:47:39 +0430

    Played around with context, redux is definitely the better choice

--- src/components/Field.js
+++ src/components/Field.js
@@ -4,7 +4,7 @@ import LanguageContexts from '../contexts/LanguageContexts'
 class Field extends React.Component {
     static contextType = LanguageContexts
     render() {
-        const text = this.context === 'english' ? 'Name' : 'Naam'
+        const text = this.context.language === 'english' ? 'Name' : 'Naam'
         return (
             <div className='ui field'>
                 <label>{text}</label>
